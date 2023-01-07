d01cda233083ae1f8901342844ac60de49fcae25
Mateusz Dudka <mateuszdudka2000@wp.pl>
2023-01-07 03:16:44 +0100

    Adding tasks, save them in localstorage and displaying them, is done

--- todo.js
+++ todo.js
@@ -1,11 +1,28 @@
 const addTaskBtn = document.querySelector(".add-task-btn");
 const inputTask = document.querySelector("#input-task");
+const listUl = document.querySelector(".list-task-ul");
 
-let todo = localStorage.getItem("todo") || []; //tworze localstorage do przechowywawania zadań
+let todo = localStorage.getItem("todolist") | []; //tworze localstorage do przechowywawania zadań
 
 addTaskBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  let inputValue = inputTask.value;
-  todo.push(inputValue);
-  localStorage.setItem("todo", JSON.stringify(todo));
+  e.preventDefault(); //zatrzymuje odświeżanie
+  let array = []; // tworzę nową tablicę
+  array = JSON.parse(localStorage.getItem("todolist")) || []; //pobieram localstorage
+  let inputValue = inputTask.value; //przypisuje do zmiennej wprowadzaną wartość
+  array.push(inputValue); // wrzucam do localstorage(tablicy) wpisaną wartość
+  localStorage.setItem("todolist", JSON.stringify(array)); //zachowuje nową wartość w localstorage
+  displayTask();
 });
+
+let displayTask = () => {
+  let toDoList = (array = JSON.parse(localStorage.getItem("todolist")) || []); //pobieram dane z localstorage
+  let liToDo = ""; //zeruje listę ul
+  toDoList.forEach((element) => {
+    liToDo += `<li> ${element} <button>Edytuj</button> <button>Usuń</button> </li> `;
+  }); //robię pętle po każdym elemencie localstorage i wyświetlam każdy element
+  listUl.innerHTML = liToDo; //przypisuje elementy do listy ul
+  inputTask.value = ""; //zeruje wartość w inpucie po dodaniu
+};
+
+let displayToDo = displayTask();
+window.onload = displayToDo; //aby po załadowaniu strony(odświeżeniu strony) wyświetlały się zadania do zrobienia
