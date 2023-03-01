ff401765a6a4660db9eae0b24a0aa750f3b6dd9d
Mateusz Dudka <mateuszdudka2000@wp.pl>
2023-03-01 13:08:18 +0100

    Adding comments describing the action

--- todo.js
+++ todo.js
@@ -1,65 +1,68 @@
 const addTaskBtn = document.querySelector(".add-task-btn");
 const inputTask = document.querySelector("#input-task");
-let listDiv = document.querySelector("#list-tasks");
+const listDiv = document.querySelector("#list-tasks"); //pobieranie elementów z htmla
 
-let todos = JSON.parse(localStorage.getItem("todolist")) || []; //tworze localstorage do przechowywawania zadań
+let todos = JSON.parse(localStorage.getItem("listTask")) || []; //tworzenie localstorage do przechowywawania zadań
 
 addTaskBtn.addEventListener("click", (e) => {
-  e.preventDefault(); //zatrzymuje odświeżanie
-  const todo = {
-    content: inputTask.value,
-  };
-  todos.push(todo); // wrzucam do localstorage(tablicy) wpisaną wartość
-  localStorage.setItem("todolist", JSON.stringify(todos)); //zapisuje nową wartość w localstorage
-
-  displayTask();
-}); // dodawanie zadań do localstorage działa
+  e.preventDefault(); //zatrzymuje odświeżanie strony
+  const todotask = {
+    task: inputTask.value,
+  }; //tworzenie obiektu w celu przypisywania do zmiennej podanego zadania
+  todos.push(todotask); // wrzucanie do localstorage(tablicy) wpisaną wartość
+  localStorage.setItem("listTask", JSON.stringify(todos)); //zapisywanie nowej wartości w localstorage
+  inputTask.value = ""; //czyszczenie inputa po dodaniu zadania
+  displayTask(); //funkcja odpowiadająca za wyświetlanie zadań
+});
 
 let displayTask = () => {
-  listDiv.innerHTML = "";
+  listDiv.innerHTML = ""; //czyszczenie listy
 
-  todos.forEach((todo) => {
-    const taskItem = document.createElement("div");
-    taskItem.classList.add("task-item");
+  todos.forEach((todotask) => {
+    const taskItem = document.createElement("div"); //tworzenie elementu htmla
+    taskItem.classList.add("task-item"); //dodawanie klasy do elementu htmla
 
-    const content = document.createElement("div"); //tworze elemeny htmla
-    const actions = document.createElement("div");
-    const edit = document.createElement("button");
-    const deleteButton = document.createElement("button");
+    const contentTask = document.createElement("div"); //tworze elementu htmla
+    const actionsDisplay = document.createElement("div");
+    const editBtn = document.createElement("button");
+    const deleteBtn = document.createElement("button");
 
-    content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
-    edit.innerHTML = "Edytuj";
-    deleteButton.innerHTML = "Usuń";
+    contentTask.innerHTML = `<input type="text" value="${todotask.task}" readonly>`; //dodanie do elementu htmla danego zadania w celu wyświetlenia w postaci inputa
+    editBtn.innerHTML = "Edytuj"; //dodanie treści przycisku
+    deleteBtn.innerHTML = "Usuń"; //dodanie treści przycisku
 
-    content.classList.add("todo-content");
-    actions.classList.add("actions");
-    edit.classList.add("edit");
-    deleteButton.classList.add("delete"); //każde ma swoją klasę
+    contentTask.classList.add("todo-content");
+    actionsDisplay.classList.add("actions");
+    editBtn.classList.add("edit");
+    deleteBtn.classList.add("delete"); //każdy element ma swoją klasę
 
-    actions.appendChild(edit);
-    actions.appendChild(deleteButton);
-    taskItem.appendChild(content);
-    taskItem.appendChild(actions); // wrzucam do htmla
+    actionsDisplay.appendChild(editBtn);
+    actionsDisplay.appendChild(deleteBtn);
+    taskItem.appendChild(contentTask);
+    taskItem.appendChild(actionsDisplay);
 
-    listDiv.appendChild(taskItem);
+    listDiv.appendChild(taskItem); // wrzucanie do htmla
 
-    edit.addEventListener("click", (e) => {
-      const input = content.querySelector("input"); //bo jest content.innerHTML = `input ......`
-      input.removeAttribute("readonly");
-      input.focus();
+    editBtn.addEventListener("click", (e) => {
+      const input = contentTask.querySelector("input"); //pobieranie inputa z content
+      input.removeAttribute("readonly"); //usuwanie atrybuty odpowiadającego tylko za odczytywanie
+      input.focus(); //uruchmienie możliwości wpisywania w inputa
       input.addEventListener("blur", (e) => {
-        input.setAttribute("readonly", true);
-        todo.content = e.target.value;
-        localStorage.setItem("todolist", JSON.stringify(todos));
-        displayTask();
+        input.setAttribute("readonly", true); //ponowne ustawienie atrybuty readonly
+        todotask.task = e.target.value; //przypisanie treści docelowego zadania
+        localStorage.setItem("listTask", JSON.stringify(todos)); //zapisanie w localstorage
+        displayTask(); //wyświetlanie
       });
     });
 
-    deleteButton.addEventListener("click", (e) => {
-      todos = todos.filter((t) => t != todo);
-      localStorage.setItem("todolist", JSON.stringify(todos));
+    deleteBtn.addEventListener("click", (e) => {
+      todos = todos.filter((t) => t != todotask); //filtracja na tablice w celu usunięcia elementów
+      localStorage.setItem("listTask", JSON.stringify(todos));
       displayTask();
     });
   });
 };
 displayTask();
+
+//todos - localstorage(tablica obiektów)
+//todotask - obiekt do którego zostaje przypisana treść zadania
